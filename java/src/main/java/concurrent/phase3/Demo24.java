3eff41b2316499171c2196f58b37fba8d437c0d9
joyang <walp_1314>
2020-08-19 20:27:01 +0800

    多线程3

--- java/src/main/java/concurrent/phase3/Demo24.java
+++ java/src/main/java/concurrent/phase3/Demo24.java
@@ -12,9 +12,11 @@ public class Demo24 {
     }
 
     public static void test1() throws InterruptedException {
+        // return new ScheduledThreadPoolExecutor(corePoolSize);
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
         Runnable task = () -> System.out.println("Scheduling: " + System.nanoTime());
+        // 延迟3s执行
         ScheduledFuture<?> future = executor.schedule(task, 3, TimeUnit.SECONDS);
 
         TimeUnit.MILLISECONDS.sleep(1337);
